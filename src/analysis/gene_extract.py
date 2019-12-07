0c5f0d6279c1a62dfe8164278a04e0de511b397d
Xin Liu <xiliu1@microsoft.com>
2019-12-07 20:54:50 +0800

    Enable extract dna by range

--- src/analysis/gene_extract.py
+++ src/analysis/gene_extract.py
@@ -1,19 +1,25 @@
+import os
+
 from utils.gene_file_util import GeneFileReader
+from utils.str_util import StrConverter
 
 
 class GeneExtract:
-    def __init__(self, data_path, rna_path, output_path, based='gene'):
+    def __init__(self, data_path, rna_path, output_directory, gene_extract_based='gene'):
         self.data_path = data_path
         self.rna_path = rna_path
-        self.output_path = output_path
-        self.based = based
+        self.output_directory = output_directory
+        file_name = os.path.basename(rna_path)
+        file_prefix = StrConverter.extract_file_name(file_name)
+        self.result_path = os.path.join(self.output_directory, '%s_extract_result.txt' % file_prefix)
+        self.gene_extract_based = gene_extract_based
         self.gene_reader = GeneFileReader(self.data_path)
 
     def run(self):
         self.gene_reader.build_information()
         dna_code = self.gene_reader.dna_code
-        with open(self.output_path, 'w') as fw:
-            if self.based == 'gene':
+        with open(self.result_path, 'w') as fw:
+            if self.gene_extract_based == 'gene':
                 fw.write('No\tgene\tfrom\t\tend\tproduct\tsequence\n')
                 for gene_idx, gene in enumerate(open(self.rna_path)):
                     gene = gene.strip()
@@ -29,5 +35,22 @@ class GeneExtract:
                             gene_idx + 1, gene, start, end, product, sequence))
                     if not succ:
                         print('%s not found in %s' % (gene, self.data_path))
-            elif self.based == 'internal':
-                raise NotImplementedError()
+            elif self.gene_extract_based == 'range':
+                for line in open(self.rna_path):
+                    fw.write(line.strip('\n'))
+                    info = line.strip().split('\t')
+                    if len(info) == 5:
+                        a, b = map(int, info[-2:])
+                        left = min(a, b)
+                        right = max(a, b)
+                        direction = a < b
+                        if direction:
+                            right += 2
+                        else:
+                            left -= 2
+                        dna = dna_code[left:right + 1]
+                        if not direction:
+                            fw.write('\t' + dna[::-1])
+                        else:
+                            fw.write('\t' + dna)
+                    fw.write('\n')
