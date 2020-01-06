338f5db6ebd155931a4186d68cd0a3726d968c3d
Xin Liu <xiliu1@microsoft.com>
2020-01-06 23:06:43 +0800

    Add missing neighbor

--- src/run_cluster_neighbor_analysis.py
+++ src/run_cluster_neighbor_analysis.py
@@ -1,4 +1,5 @@
 from analysis.cluster_match import ClusterMatcher
+from analysis.neighbor_analysis import NeighborAnalysis
 from experiment_config import *
 
 fna_name = 'T2_sun_bacteria.fna'
@@ -8,3 +9,7 @@ if __name__ == '__main__':
     input_path = os.path.join(data_directory, fna_name)
     cluster_match = ClusterMatcher(rna_tag, input_path, output_directory)
     cluster_match.run()
+    file_name = rna_tag + '_all_result.txt'
+    input_path = os.path.join(output_directory, file_name)
+    neighbor_analysis = NeighborAnalysis(input_path, rna_download_directory, output_directory)
+    neighbor_analysis.run()
