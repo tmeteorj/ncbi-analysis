9d2e9a9b32bb1367561c2b69f00bc501c3e5b994
Xin Liu <xiliu1@microsoft.com>
2020-01-06 22:13:45 +0800

    Extract region from dna data source

--- src/analysis/gene_range_analysis.py
+++ src/analysis/gene_range_analysis.py
@@ -5,11 +5,10 @@ from utils.str_util import StrConverter
 
 
 class GeneRangeExtract:
-    def __init__(self, data_path, range_path, output_directory):
+    def __init__(self, data_path, output_directory):
         self.data_path = data_path
-        self.range_path = range_path
         self.output_directory = output_directory
-        file_name = os.path.basename(range_path)
+        file_name = os.path.basename(data_path)
         file_prefix = StrConverter.extract_file_name(file_name)
         self.result_path = os.path.join(self.output_directory, '%s_range_result.txt' % file_prefix)
         self.gene_reader = GeneFileReader(self.data_path)
@@ -21,21 +20,15 @@ class GeneRangeExtract:
 
     def run(self):
         self.gene_reader.build_information()
-        dna_code = self.gene_reader.dna_code
         with open(self.result_path, 'w', encoding='utf8') as fw:
-            self.extract_range(dna_code, fw)
-
-    def extract_range(self, dna_code, fw):
-        lines = [line.strip() for line in open(self.range_path, 'r', encoding='utf8')]
-        last_end = 0
-        gene_idx = 0
-        fw.write('name\trange\n')
-        for line in lines[1:]:
-            items = line.split('\t')
-            locus = items[0]
-            locus = locus.split(':')[1]
-            left, right = map(int, locus.split('-'))
-            if last_end < left - 1:
-                gene_idx += 1
-                fw.write('gene_%d\tNC_000913.3:%d-%d\n' % (gene_idx, last_end, left - 1))
-            last_end = right
+            last_end = 0
+            gene_idx = 0
+            fw.write('name\trange\n')
+            for gene_segment in self.gene_reader.gene_segments:
+                left, right = gene_segment.cds
+                if last_end < left - 1:
+                    gene_idx += 1
+                    fw.write('region_%d\tNC_000913.3:%d-%d\n' % (gene_idx, last_end + 1, left - 1))
+                if left > right:
+                    print(gene_segment)
+                last_end = right
--- src/run_analysis.py
+++ src/run_analysis.py
@@ -34,7 +34,6 @@ output_best_promoter = True
 cookie = 'windowOrg=ptools0%3AECOLI%3Aptools1%3AECOLI%3A; recentOrgID0=ECOLI; pagecount=16; frameHeight=759; JSESSIONID=1D257C680D66CA41ED0FC9432FD0BD3E; frameWidth=1500; _gat=1; _gid=GA1.2.610587971.1577521867; PTools-session=biocyc14b~biocyc14-3786098971%7CNIL%20NIL%20%22%22%2042107%200%20(%3AWEB%20NIL%20-1%20((%3ABASICS%20-1)%20(%3AQUERIES%20-1)%20(%3AADVANCED%20-1)))%20NIL%20NIL%20ECOBASE%20NIL%20NIL%20%7Cd0vt8w4rc07g29ilyr3o518e74ero6c; _ga=GA1.2.407871027.1577110083; credentialId=218865; secretKey=27oXO8IVRHh01SA3ae/qL9Yqfwk='
 do_ecocyc_analysis = False
 
-nc_range_file_name = 'gene_nc_range.txt'
 nc_gene_file_name = 'NC_000913.3.txt'
 do_nc_range_extract = True
 
@@ -76,8 +75,7 @@ def run_ecocyc_analysis():
 
 def run_nc_range_extract():
     data_path = os.path.join(rna_download_directory, nc_gene_file_name)
-    range_path = os.path.join(data_directory, nc_range_file_name)
-    gene_range_extract = GeneRangeExtract(data_path, range_path, output_directory)
+    gene_range_extract = GeneRangeExtract(data_path, output_directory)
     gene_range_extract.run()
 
 
