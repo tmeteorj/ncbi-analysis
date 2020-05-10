d1def7ecb4a1705e73fa96c7b61c299e164e8c60
Xin Liu <xiliu1@microsoft.com>
2020-05-10 18:03:41 +0800

    - More detail information when running - Add batch size control

--- src/run_gene_similarity_match.py
+++ src/run_gene_similarity_match.py
@@ -4,12 +4,13 @@ from analysis.gene_similarity_match import GeneSimilarityMatch
 from analysis.neighbor_analysis import NeighborAnalysis
 from experiment_config import ExperimentConfig
 
-gene_match_names = ['agrB.txt']
+gene_match_names = ['dinQ.txt']
 data_name = 'NC_000913.3.txt'
 match_algorithm = 'text_distance'
 # match_algorithm = 'char_match'
 candidate_distance = 5
-top_k = 50
+top_k = 30
+batch_size = 2
 
 if __name__ == '__main__':
     for gene_match_name in gene_match_names:
@@ -18,7 +19,8 @@ if __name__ == '__main__':
         similarity_match = GeneSimilarityMatch(gene_path, data_path, ExperimentConfig.output_directory,
                                                top_k=top_k,
                                                match_algorithm=match_algorithm,
-                                               candidate_distance=candidate_distance)
+                                               candidate_distance=candidate_distance,
+                                               batch_size=batch_size)
         similarity_match.run()
 
         neighbor_analysis = NeighborAnalysis(similarity_match.result_path, ExperimentConfig.rna_download_directory,
