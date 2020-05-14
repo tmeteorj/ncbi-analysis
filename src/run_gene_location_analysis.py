2efc665e7fa922ae49972ff2cedcdfb5d29f3e85
Xin Liu <tmeteorj@gmail.com>
2020-05-14 20:27:58 +0800

    Location analysis (#32)
    * Add database loader for ecocyc
    
    * Add interval check
    
    * Add direction
    
    * Update format
    
    Co-authored-by: Xin Liu <xiliu1@microsoft.com>

---
+++ src/run_gene_location_analysis.py
@@ -0,0 +1,15 @@
+import os
+
+from analysis.gene_location_analysis import GeneLocationAnalysis
+from experiment_config import ExperimentConfig
+
+location_analysis_names = ['dinQ_antisense_consistency_consistency_match_result.txt']
+ecocyc_file_name = 'Ecocyc_NC_000913.txt'
+
+if __name__ == '__main__':
+    ecocyc_file_path = os.path.join(ExperimentConfig.data_directory, ecocyc_file_name)
+    for input_file_name in location_analysis_names:
+        input_file_path = os.path.join(ExperimentConfig.data_directory, input_file_name)
+        gene_location_analysis = GeneLocationAnalysis(input_file_path, ecocyc_file_path,
+                                                      ExperimentConfig.output_directory)
+        gene_location_analysis.run()
---
+++ src/utils/ecocyc_data_loader.py
@@ -0,0 +1,86 @@
+class EcocycDataLoader:
+    def __init__(self, ecocyc_data_file):
+        self.ecocyc_data_file = ecocyc_data_file
+        self.records = []
+        self.inter_records = []
+        self.headers = None
+        self.inv_headers = []
+
+    def build_database(self):
+        for line in open(self.ecocyc_data_file, 'r', encoding='utf8'):
+            items = line.rstrip('\r\n').split('\t')
+            if len(items) == 0:
+                continue
+            if self.headers is None:
+                self.headers = {}
+                for idx, header in enumerate(items):
+                    self.headers[header] = idx
+                    self.inv_headers.append(header)
+                continue
+            attr = {header: value for header, value in zip(self.inv_headers, items)}
+            record = EcocycRecord(attr)
+            direction, inter_records = record.generate_inter_record()
+            self.records.append(record)
+            self.inter_records.extend(inter_records)
+        self.inter_records.sort(key=lambda arg: arg.start)
+
+    def find_first_le(self, pos):
+        return binary_search_first_le(self.inter_records,
+                                      0,
+                                      len(self.inter_records) - 1,
+                                      pos)
+
+
+def binary_search_first_le(arr, left, right, value):
+    while left < right:
+        mid = (left + right) // 2
+        if arr[mid].start >= value:
+            right = mid
+        elif arr[mid].start < value:
+            left = mid + 1
+    return left
+
+
+class EcocycRecord:
+    def __init__(self, attr):
+        for header in ['gene', 'product', 'promoter_name', 'promoter_pos', 'gene_start_pos', 'map_start_pos',
+                       'map_end_pos']:
+            if header.endswith('pos'):
+                value = attr.get(header, -1)
+                if value == '':
+                    value = -1
+                setattr(self, header, int(value))
+            else:
+                setattr(self, header, attr.get(header, ''))
+
+    def generate_inter_record(self):
+        records = []
+        records.append(
+            EcocycInterRecord(name=self.gene,
+                              product=self.product,
+                              start=self.map_start_pos,
+                              end=self.map_end_pos,
+                              is_gene=True)
+        )
+        if self.promoter_name != '':
+            records.append(
+                EcocycInterRecord(name=self.promoter_name,
+                                  product='',
+                                  start=self.promoter_pos,
+                                  end=self.gene_start_pos,
+                                  is_gene=False)
+            )
+        return records[-1].direction, records
+
+
+class EcocycInterRecord:
+    def __init__(self, name, product, start, end, is_gene):
+        self.name = name
+        self.product = product
+        self.start = start
+        self.end = end
+        self.direction = '>' if start < end else '<'
+        self.is_gene = is_gene
+
+        self.left = min(self.start, self.end)
+        self.right = max(self.start, self.end)
