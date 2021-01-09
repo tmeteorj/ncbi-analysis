0bb12f7e3e72aa6ede5c247f68a6f303d69a0362
Xin Liu <xiliu1@microsoft.com>
2021-01-09 11:14:37 +0800

    Update number generate

--- src/run_random_generate.py
+++ src/run_random_generate.py
@@ -1,7 +1,19 @@
+import os
 import random
 
-target = 10
+from experiment_config import ExperimentConfig
+from utils.str_util import StrConverter
+
+input_file_name = 'number.txt'
 
 if __name__ == '__main__':
-    ls = [(19.5 + random.random()) / 20 * target for n in range(3)]
-    print('\t'.join(map(str, ls)))
+    input_file_path = os.path.join(ExperimentConfig.data_directory, input_file_name)
+    file_name = os.path.basename(input_file_path)
+    file_prefix = StrConverter.extract_file_name(file_name)
+    result_path = os.path.join(ExperimentConfig.output_directory,
+                               '%s_number_result.txt' % file_prefix)
+    with open(result_path, 'w') as f:
+        for target in open(input_file_path, 'r'):
+            target = int(target.strip())
+            ls = [(19.5 + random.random()) / 20 * target for n in range(3)]
+            f.write('\t'.join(map(str, ls)) + '\n')
