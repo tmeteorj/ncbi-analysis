6abf1b3aa5971ab7da8c1f3ca40716aab34cf77e
Xin Liu <tmeteorj@gmail.com>
2021-01-12 21:57:30 +0800

    Extract name too (#41)
    Co-authored-by: Xin Liu <xiliu1@microsoft.com>

--- src/5-run_kegg.py
+++ src/5-run_kegg.py
@@ -3,9 +3,9 @@ import os
 from analysis.kegg_analysis import KeggAnalysis
 from experiment_config import ExperimentConfig
 
-# input_file_name = '20200103.txt'
-input_file_name = "kegg_genes.txt"
-is_gene = True
+input_file_name = '20210112.txt'
+# input_file_name = "kegg_genes.txt"
+is_gene = False
 
 if __name__ == '__main__':
     input_file_path = os.path.join(ExperimentConfig.data_directory, input_file_name)
